e0e51accd4ab3b584b4a62a26fefc394b17346a1
Gerrae Cotton <gerrae.cotton@gmail.com>
2016-08-01 15:11:57 -0400

    Adding the option to have more than one player

--- src/lab8/BattingStatistics.java
+++ src/lab8/BattingStatistics.java
@@ -7,38 +7,70 @@ public class BattingStatistics {
 
 	public static void main(String[] args) {
 		Scanner scan1 = new Scanner(System.in);
-		System.out.println("please enter the number of at bats");
-		int numOfBats = scan1.nextInt();
+		System.out.println("How many players are there?");
+		int numOfPlayers = scan1.nextInt();
 		scan1.nextLine();
-		int[] atBatResults = new int[numOfBats];
+		int numOfBats = 0;
 		int result = 0;
 		int totalNumOfAtBatsEarningAtLeast1 = 0;
 		int totalNumOfBasesEarned = 0;
-		for (int i = 0; i < numOfBats; i++) {
-			System.out
-					.println("Enter the at number of bases earned for at bat #"
-							+ (i + 1));
-			result = scan1.nextInt();
-			scan1.nextLine();
-			atBatResults[i] = result;
-			totalNumOfBasesEarned = totalNumOfBasesEarned + result;
-			if (result >= 1) {
-				totalNumOfAtBatsEarningAtLeast1++;
-			}
-		}
-
+		int[][] atBatResults = new int[numOfPlayers][];
 		DecimalFormat df = new DecimalFormat("#.000");
 		double battingAvg = 0.0;
 		double sluggingPercent = 0;
-		System.out.println(totalNumOfAtBatsEarningAtLeast1);
-		System.out.println(totalNumOfBasesEarned);
-		System.out.println(numOfBats);
-		battingAvg = totalNumOfAtBatsEarningAtLeast1 / (double) numOfBats;
-		System.out.println(df.format(battingAvg));
-		sluggingPercent = totalNumOfBasesEarned / (double) numOfBats;
-		System.out.println(df.format(sluggingPercent));
+		for (int j = 1; j <= numOfPlayers; j++) {
+			System.out.println("Please enter the number of at bats for Player "
+					+ j);
+			numOfBats = scan1.nextInt();
+			scan1.nextLine();
+
+			atBatResults[j - 1] = new int[numOfBats];
+
+			for (int i = 0; i < numOfBats; i++) {
+				System.out
+						.println("Enter the at number of bases earned for player "
+								+ j + " at bat #" + (i + 1));
+				result = scan1.nextInt();
+				scan1.nextLine();
+				atBatResults[j - 1][i] = result;
+				
+				//totalNumOfBasesEarned = totalNumOfBasesEarned + result;
+				//if (result >= 1) { 
+				//totalNumOfAtBatsEarningAtLeast1++;
+				//}
+				 
+
+			}
+			result = 0;
+			/*battingAvg = totalNumOfAtBatsEarningAtLeast1 / (double) numOfBats;
+			System.out.println(df.format(battingAvg));
+			sluggingPercent = totalNumOfBasesEarned / (double) numOfBats;
+			System.out.println(df.format(sluggingPercent));
+			System.out.println(atBatResults.length);*/
+		}
+
+		// for(int bat : atBatResults){
 
 		scan1.close();
-	}
 
+		int sum = 0;
+		int count = 0;
+		 for (int k = 0; k <=numOfPlayers - 1; k++){
+		 
+		for(int l = 0; l < atBatResults[k].length; l++){
+		    sum += atBatResults[k][l];
+		    if(atBatResults[k][l] >= 1){
+		    	count++;
+		    }
+		}
+		sluggingPercent = sum/(double)numOfBats; 
+		battingAvg = count/(double)numOfBats;
+		//System.out.println(sum);
+		//System.out.println(numOfBats);
+		System.out.println("The slugging % for Player" + (k+1) + " is " + df.format(sluggingPercent)+ "and the batting average is " + df.format(battingAvg));
+		sum=0;
+		 }
+		
+		
+	}
 }
